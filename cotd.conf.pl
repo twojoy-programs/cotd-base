0bc86b95b3905b407c2d3ee8a9c18a527f0e447e
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-14 10:16:47 -0700

    Made exapmple dev config file.

--- cotd.conf.pl
+++ cotd.conf.pl
@@ -1,9 +1,9 @@
-
 # Example config file for the COTD program.
+# this is for a local dev environment.
+use Env;
 '
 Copyright (c) 2016, Tucker R. Twomey
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
     * Redistributions of source code must retain the above copyright
@@ -26,3 +26,11 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ';
+
+$logfile     = `pwd` . "/cotd.log"; # Unportable
+
+$sounds      = `pwd` . "/sounds";   # ditto
+
+$numSounds   = 100
+
+$reportsounds= 1
